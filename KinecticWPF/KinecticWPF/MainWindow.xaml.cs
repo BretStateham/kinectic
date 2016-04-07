a1fb75d4e6088f589fd5f51e4cd29d5e321e2b42
BretStateham <Bret.Stateham@microsoft.com>
2016-04-06 18:48:17 -0700

    KinectRegion Implementation and Logo Creation

---
+++ KinecticWPF/KinecticWPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Kinect;
+using Microsoft.Kinect.Wpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kinectic
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    #region Fields
+
+    KinectSensor sensor;
+
+    #endregion Fields
+
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      InitKinectRegion();
+    }
+
+    private void InitKinectRegion()
+    {
+      //Give the UserViewer a way to find the KinectRegion
+      KinectRegion.SetKinectRegion(this, kinectRegion);
+
+      //Wire up the current connect region to the App level KinectRegion
+      App app = ((App)Application.Current);
+      app.KinectRegion = kinectRegion;
+
+      //Get the default KinectSensor
+      sensor = KinectSensor.GetDefault();
+
+      //Assign it to the KinectRegion
+      this.kinectRegion.KinectSensor = sensor;
+
+      //And open it
+      sensor.Open();
+    }
+  }
+}
