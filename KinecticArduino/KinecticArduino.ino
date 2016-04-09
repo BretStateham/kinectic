d027f60c7202d760ca1864a1a486e50b43fc00c7
BretStateham <Bret.Stateham@microsoft.com>
2016-04-09 00:16:43 -0700

    Added WS2801 LED Strip Support to Arduino

--- KinecticArduino/KinecticArduino.ino
+++ KinecticArduino/KinecticArduino.ino
@@ -1,3 +1,9 @@
+#include <Adafruit_WS2801.h>
+#include "SPI.h" // Comment out this line if using Trinket or Gemma
+#ifdef __AVR_ATtiny85__
+ #include <avr/power.h>
+#endif
+
 /*
 
 KinecticArduino
@@ -37,6 +43,12 @@ If it receives a command it recognizes, it responsds with that same command
 
 */
 
+uint8_t dataPin  = 2;    // Yellow wire on Adafruit Pixels
+uint8_t clockPin = 3;    // Green wire on Adafruit Pixels
+
+// Set the first variable to the NUMBER of pixels. 20 = 20 pixels in a row
+Adafruit_WS2801 strip = Adafruit_WS2801(20, dataPin, clockPin);
+
 #define RELAYON  0  //Arduino pin needs to be LOW (0) to turn the relay on
 #define RELAYOFF 1  //Arduino pin needs to be HIGH (1) to turn the relay off
 
@@ -57,8 +69,21 @@ void setup() {
   pinMode(lightPin,OUTPUT);
   pinMode(bellPin,OUTPUT);
 
+  InitLedStrip();
+
   writePins();
 
+}
+
+void InitLedStrip(){
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+    clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+  #endif
+
+  strip.begin();
+
+  // Update LED contents, to start they are all 'off'
+  strip.show();
 }
 
 void writePins(){
@@ -89,6 +114,12 @@ void loop() {
     //We don't care about any other values
     switch(inByte)
     {
+      case '0':
+        Serial.println('0');
+        lightPinState = RELAYOFF;
+        bellPinState = RELAYOFF;
+        colorWipe(Color(0,0,0),2);
+        break;
       case 'l':
         Serial.println('l');
         lightPinState = RELAYOFF;
@@ -105,6 +136,17 @@ void loop() {
         Serial.println('B');
         bellPinState = RELAYON;
         break;
+      case 's':
+        Serial.println('s');
+        colorWipe(Color(0,0,0),0);
+        break;
+      case 'S':
+        Serial.println('S');
+        colorWipe(Color(255,0,0),10);
+        colorWipe(Color(0,255,0),10);
+        colorWipe(Color(0,0,255),10);
+        colorWipe(Color(255,255,255),10);
+        break;
       case 'A':
         Serial.println('A');
         Alert(3);
@@ -112,5 +154,76 @@ void loop() {
     }
     writePins();
   }
+}
 
+// Methods used by the Adafruit LED Strip:
+void rainbow(uint8_t wait) {
+  int i, j;
+   
+  for (j=0; j < 256; j++) {     // 3 cycles of all 256 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel( (i + j) % 255));
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+// Slightly different, this one makes the rainbow wheel equally distributed 
+// along the chain
+void rainbowCycle(uint8_t wait) {
+  int i, j;
+  
+  for (j=0; j < 256 * 5; j++) {     // 5 cycles of all 25 colors in the wheel
+    for (i=0; i < strip.numPixels(); i++) {
+      // tricky math! we use each pixel as a fraction of the full 96-color wheel
+      // (thats the i / strip.numPixels() part)
+      // Then add in j which makes the colors go around per pixel
+      // the % 96 is to make the wheel cycle around
+      strip.setPixelColor(i, Wheel( ((i * 256 / strip.numPixels()) + j) % 256) );
+    }  
+    strip.show();   // write all the pixels out
+    delay(wait);
+  }
+}
+
+// fill the dots one after the other with said color
+// good for testing purposes
+void colorWipe(uint32_t c, uint8_t wait) {
+  int i;
+  
+  for (i=0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, c);
+      strip.show();
+      delay(wait);
+  }
+}
+
+/* Helper functions */
+
+// Create a 24 bit color value from R,G,B
+uint32_t Color(byte r, byte g, byte b)
+{
+  uint32_t c;
+  c = r;
+  c <<= 8;
+  c |= g;
+  c <<= 8;
+  c |= b;
+  return c;
+}
+
+//Input a value 0 to 255 to get a color value.
+//The colours are a transition r - g -b - back to r
+uint32_t Wheel(byte WheelPos)
+{
+  if (WheelPos < 85) {
+   return Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if (WheelPos < 170) {
+   WheelPos -= 85;
+   return Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170; 
+   return Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
 }
